27ba540c77d03e9dd63d1869d25978ec71077a47
Kaliyah Fisher <61337314+KaliyahFisher@users.noreply.github.com>
2022-11-28 11:02:38 -0600

    Added lesson/student maintenance pages

---
+++ LEAP_DB_Web/Pages/Shared/LessonPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LEAP_DB_Web.Pages.Shared
+{
+    public class LessonPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
