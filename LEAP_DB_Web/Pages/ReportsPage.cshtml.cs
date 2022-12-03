eb897b5baeee31938a700a9f005511de4c208242
bee0018 <91974239+bee0018@users.noreply.github.com>
2022-12-02 21:54:59 -0600

    Replaced Contact Us card with reporting system

---
+++ LEAP_DB_Web/Pages/ReportsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LEAP_DB_Web.Pages
+{
+    public class ReportsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
