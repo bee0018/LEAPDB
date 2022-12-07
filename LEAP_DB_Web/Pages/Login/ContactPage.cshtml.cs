b7a559110f7eecfe8b4b262da280c54829d518b7
bee0018 <91974239+bee0018@users.noreply.github.com>
2022-12-07 00:01:47 -0600

    Added the email capability to send mail to numerous inboxes
    I also made a change to the code behind and the file structure of the project moving forward

--- LEAP_DB_Web/Pages/Login/ContactPage.cshtml.cs
+++ LEAP_DB_Web/Pages/Login/ContactPage.cshtml.cs
@@ -1,3 +1,4 @@
+using LEAP_DB_Web.Pages.Login.Util;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,6 +8,18 @@ namespace LEAP_DB_Web.Pages.Login
     {
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            // Gathers the fields on Post
+            var toAddress = Request.Form["emailAddress"];
+            var subject = Request.Form["subject"];
+            var message = Request.Form["message"];
+
+            // Sends the email to the user
+            EmailUtil.SendEmail(toAddress, subject, message);
+
         }
     }
 }
---
+++ LEAP_DB_Web/Pages/Login/Util/EmailUtil.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace LEAP_DB_Web.Pages.Login.Util
+{
+    public class EmailUtil
+    {
+        // Email placeholder 
+        private static string GetUserName()
+        {
+            return "csseaffinity.noreply@gmail.com";
+        }
+
+        // Email Password placeholder
+        private static string GetPassword()
+        {
+            return "dqhxjarjmqfzfieu";
+        }
+
+        /// <summary>
+        /// Sends the email to the user of choice with a custom message.
+        /// </summary>
+        /// <param name="toAddress"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        public static void SendEmail(string toAddress, string subject, string body)
+        {
+            // Constructs email object
+            var email = new SmtpClient
+            {
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Host = "smtp.gmail.com",
+                Port = 587,
+                Credentials = new NetworkCredential(GetUserName(), GetPassword())
+            };
+
+            // Attempts to send email address
+            try
+            {
+                email.Send(GetUserName(), toAddress, subject, body);
+                Console.WriteLine("\nEmail sent successfully!");
+            }
+            catch (SmtpException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
+
